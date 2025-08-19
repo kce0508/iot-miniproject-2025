21177955ceffb515b86778d74b1e1683d6f79dd8
kce0508 <kce0508@gmail.com>
2025-08-19 16:53:07 +0900

    미니프로젝트3

---
+++ miniproject3/mes/MesMiniproject/WpfIoTSimulatorApp/Models/PrcMsg.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfIoTSimulatorApp.Models
+{
+    public class PrcMsg
+    {
+        public string ClientId { get; set; }
+        public string PlantCode { get; set; }
+        public string FacilityId { get; set; }
+        public string Flag { get; set; }
+    }
+}
---
+++ miniproject3/mes/MesMiniproject/WpfMrpSimulatorApp/Models/CheckResult.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfMrpSimulatorApp.Models
+{
+    // JSON 전송용 객체. 딴데 안써요
+    public class CheckResult
+    {
+        public string ClientId { get; set; }
+        public string Timestamp { get; set; }
+        public string Result { get; set; }
+    }
+}
